2f2ca759f57c3f67a0c0b71ec3e83b390521d384
Allure <samira-h@hotmail.co.uk>
2022-01-15 23:51:20 +0000

    css edit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # code-quiz
+The motivation behind this code quiz is to be able to make it interactive and set a timer when the user starts the quiz.
+
+Questions and answers is inside the consule.log
+
+Installation
+To access this project on GitHub you can follow these links:
+
+GitHub repository:    git@github.com:samira0101/code-quiz.git
+
+GitHub url:   https://github.com/samira0101/code-quiz.git
+
+Refrences
+
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+
+https://stackoverflow.com/questions/42080365/using-addeventlistener-and-getelementsbyclassname-to-pass-an-element-id
+
+https://stackoverflow.com/questions/1472705/resetting-a-settimeout
---
+++ script.js
@@ -0,0 +1,92 @@
+const startingMinutes = 5;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('timer');
+
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+   
+}
+
+let quizData = [
+    {
+     question:"What does CSS stand for?",
+     multiplechoice: [
+         "Computer style sheets", 
+         "Colourful style sheets",
+         "Cascading style sheets"
+     ],
+     answer: "Cascading style sheets"
+    },
+    {
+        question:"What does HTML stand for?",
+        multiplechoice: [
+            "Hyper text markup langauge", 
+            "Hyperlinks and text markup language",
+            "Home tool markup"
+        ],
+        answer: "Home tool markup"
+       },
+    {
+        question:"Inside which HTML element do we put the Javascript code",
+        multiplechoice: [
+            "Script", 
+            "Javascript",
+            "JS"
+        ],
+        answer: "Script"
+       },
+       {
+        question:"Can you reassign a const variable",
+        multiplechoice: [
+            "Yes", 
+            "No",
+        ],
+        answer: "No"
+       }
+   
+
+];
+
+let question = 0;
+let userAnswer;
+let currentQuestion = document.getElementById("question");
+let startButton = document.getElementById('start-button');
+let multiplechoice = document.getElementById("multipleChoices");
+let choice = document.getElementsByClassName("choice");
+
+
+
+
+startButton.addEventListener('click' , () => { 
+    let interval = setInterval(updateCountdown, 1000);
+    let list = ""
+
+    currentQuestion.innerHTML = quizData[question].question;
+
+    for(let i = 0; i < quizData[question].multiplechoice.length; i++){
+        list += `<li class="choice">${quizData[question].multiplechoice[i]} </li>`
+    }
+
+    multiplechoice.innerHTML = list;
+
+    for(let i = 0 ; i < choice.length ; i++){
+        choice[i].addEventListener('click', () => {
+            userAnswer = choice[i].innerText;
+            console.log(`Your answer: ${userAnswer}`)
+            console.log(`The correct answer is: ${quizData[question - 1].answer}`)
+            console.log(`The answer is: ${userAnswer == quizData[question - 1].answer}`)
+        })
+    }
+
+    question++;
+    clearInterval(interval);
+
+
+});
+
